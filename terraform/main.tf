7fc68aedd36268795c7d839c6fc66bfc38939d12
Joao Leao <Joao.Leao@criticaltechworks.com>
2022-11-17 14:57:15 +0000

    update terraform

--- terraform/main.tf
+++ terraform/main.tf
@@ -52,8 +52,19 @@ resource "azurerm_network_security_group" "myterraformnetworksecuritygroup" {
     destination_address_prefix = "*"
   }
 
-security_rule {
+  security_rule {
+    name = "HTTP"
+    priority = 1005
+    direction = "Inbound"
+    access = "Allow"
+    protocol = "Tcp"
+    source_port_range = "*"
+    destination_port_range = "80"
+    source_address_prefix = "*"
+    destination_address_prefix = "*"
+  }
 
+  security_rule {
     name                       = "Prometheus"
     priority                   = 1020
     direction                  = "Inbound"
